85bee7d7a86655241687e7cbf2343c30e5faaa67
Lucas Teng <lucas.teng@ryerson.ca>
2016-03-18 18:41:29 -0400

    Added Character Stage
    added character stage, and now checks for redundant character/stage matches

--- Table/table_creation.sql
+++ Table/table_creation.sql
@@ -13,18 +13,19 @@ CREATE TABLE IF NOT EXISTS features
         id MEDIUMINT NOT NULL AUTO_INCREMENT,
         HEXid char(6) unique,
         sprite_filename TEXT,
+        station_id TEXT,
         PRIMARY KEY (id)
         ) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
 
 CREATE TABLE IF NOT EXISTS visits
         (
-        id MEDIUMINT NOT NULL AUTO_INCREMENT,
         date_posted datetime NOT NULL default '0000-00-00 00:00:00',
+        current_state TINYINT,
         character_ID   char(6),
         feature_ID     char(6),
         svg_path TEXT,
         audio_path TEXT,
         FOREIGN KEY (character_ID)  REFERENCES characters(HEXid),
         FOREIGN KEY (feature_ID)      REFERENCES features(HEXid),
-        PRIMARY KEY (id)
+        PRIMARY KEY (character_ID, current_state)
         ) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
--- controllers/update_character.controller.php
+++ controllers/update_character.controller.php
@@ -7,6 +7,10 @@
 
 $methods = array();
 
+$methods['error'] = function($instance){
+	echo '{"error": "Something went wrong"}';
+};
+
 $methods['run'] = function($instance) {
 	// Set headers
 	header('Content-Type: application/json');
@@ -21,22 +25,23 @@ $methods['run'] = function($instance) {
 
 	// === Generate current character === //
 
-	$sql = "SELECT c.HEXid, c.pri_color, c.sec_color, v.id, f.sprite_filename FROM characters c
+	// detect if there is a character already with this ID
+	$sql = "SELECT c.HEXid v.current_state FROM characters c
 	LEFT JOIN visits v ON v.character_ID=c.HEXid
-	LEFT JOIN features f ON v.feature_ID=f.HEXid
 	WHERE c.HEXid=:charid
 	";
 	// Prepare statement
 	$stmt = $pdo->prepare($sql);
 	// Bind values
 	$stmt->bindValue("charid",  $characterID,  PDO::PARAM_STR );
-	// Do the thing
 	$stmt->execute();
 	// Fetch results into associative array
 	$result = array();
 	while ( $row = $stmt->fetch(PDO::FETCH_ASSOC) ) {
 		$result[] = $row;
 	}
+	$current_state = count($result)+1;
+	//if there is no character with this ID, create it
 	if(count($result) < 1){
 		$sql = "INSERT INTO characters (HEXid, pri_color, sec_color, date_created)
 		VALUES (:HXid, :pricol, :seccol, now())";
@@ -52,32 +57,61 @@ $methods['run'] = function($instance) {
 	}
 
 	// === Modify character for visit === ///
+	$sql = "SELECT f.HEXid FROM features f
+	WHERE f.station_id=:stnID ORDER BY rand()
+	limit 1
+	";
+	// Prepare statement
+	$stmt = $pdo->prepare($sql);
+	// Bind values
+	$stmt->bindValue("stnID",  $stationID,  PDO::PARAM_STR );
+	$stmt->execute();
+	// Fetch results into associative array
+	$feature_ID = null;
+	if($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+		$feature_ID = $row["HEXid"];
+	} else {
+		$methods["error"];
+	}
 
-	$sql = "INSERT INTO visits (character_ID, feature_ID, date_posted)
-	VALUES (:charid, :featid, now())";
+	$sql = "INSERT INTO visits (character_ID, feature_ID, current_state, date_posted)
+	VALUES (:charid, :featid, :state, now())";
 	$stmt = $pdo->prepare($sql);
 	// Bind variables
-	$stmt->bindValue("charid", $characterID, PDO::PARAM_STR );
-	$stmt->bindValue("featid", $stationID,  PDO::PARAM_STR );
+	$stmt->bindValue("charid", $characterID, PDO::PARAM_STR);
+	$stmt->bindValue("featid", $stationID,  PDO::PARAM_STR);
+	$stmt->bindValue("state", $current_state,  PDO::PARAM_STR);
 	// Insert the row
 	$stmt->execute();
 	// Get the id of what we just inserted
 	$idInserted = $pdo->lastInsertId();
 
+	//get result from db
+	$sql = "SELECT c.HEXid, c.pri_color, c.sec_color, f.sprite_filename FROM characters c
+	LEFT JOIN visits v ON v.character_ID=c.HEXid
+	LEFT JOIN features f ON v.feature_ID=f.HEXid
+	WHERE c.HEXid=:charid
+	";
+	// Prepare statement
+	$stmt = $pdo->prepare($sql);
+	// Bind values
+	$stmt->bindValue("charid",  $characterID,  PDO::PARAM_STR );
+	// Do the thing
+	$stmt->execute();
+	// Fetch results into associative array
+	$result = array();
+	while ( $row = $stmt->fetch(PDO::FETCH_ASSOC) ) {
+		$result[] = $row;
+	}
 
 
 	// Print results to a temporary file for debugging
 	ob_start();
 		print_r($result);
 	file_put_contents("output.txt", ob_get_clean());
-
-	// ... you'll need to do something with the results to make the array
-	// that gets sent
-
-	// Generate testing 
-
-	// Send data
 	ob_get_clean();
+
+	// make JSON object of result, and print that
 	echo json_encode( $result, JSON_FORCE_OBJECT );
 };
 
